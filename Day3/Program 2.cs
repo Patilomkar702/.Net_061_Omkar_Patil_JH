03c1986c34d76f23368d4656172a735909f5c625
Patilomkar702 <patilomkar702@gmail.com>
2020-12-04 22:50:33 +0530

    Assignment 3

---
+++ Day3/Program 2.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CDAC
+{
+    class Program
+    {
+        public static int[][][] insertData()
+        {
+            Console.WriteLine("Enter Number of Batches for CDAC");
+            int bacth = Convert.ToInt32(Console.ReadLine());
+            int[][][] cdac = new int[bacth][][];
+            for (int batchno = 0; batchno < bacth; batchno++)
+            {
+                Console.WriteLine("Enter Number of Student for Batch {0}", batchno + 1);
+                int students = Convert.ToInt32(Console.ReadLine());
+                int[][] arrStudents = new int[students][];
+                for (int studentno = 0; studentno < students; studentno++)
+                {
+                    int[] arrSubject = new int[3];
+                    for (int subjetNo = 0; subjetNo < arrSubject.Length; subjetNo++)
+                    {
+                        Console.Write("Enter Marks of Studen {0} for Subject {1} ", studentno + 1, subjetNo + 1);
+                        int subMarks = Convert.ToInt32(Console.ReadLine());
+                        arrSubject[subjetNo] = subMarks;
+
+                    }
+                    arrStudents[studentno] = arrSubject;
+                    Console.WriteLine();
+
+                }
+                cdac[batchno] = arrStudents;
+                Console.Clear();
+            }
+            return cdac;
+        }
+
+        public static void display(int[][][] cdac)
+        {
+            for (int i = 0; i < cdac.Length; i++)
+            {
+                Console.WriteLine("Batch {0}", i + 1);
+                for (int j = 0; j < cdac[i].Length; j++)
+                {
+                    Console.WriteLine("Marks for Student {0}", j + 1);
+                    for (int k = 0; k < cdac[i][j].Length; k++)
+                    {
+                        Console.Write(cdac[i][j][k] + " ");
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[][][] cdac = insertData();
+            display(cdac);
+        }
+    }
+}
---
+++ Day3/Program1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace AssignmentNo3
+{
+
+    public class Emplyee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int Salary { get; set; }
+
+        public Emplyee(int id, string name = "none", int salary = 1000)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Salary = salary;
+        }
+
+        public override string ToString()
+        {
+            return "Employee ID : " + this.Id + " Name : " + this.Name + " Salary : " + this.Salary;
+        }
+
+    }
+    public class Program
+    {
+        public static void HishestSalaryEmployee(Emplyee[] arr)
+        {
+            //Display Employee With highest salary
+
+            Emplyee eMaxSal = arr[0];
+            for (int j = 0; j < arr.Length; j++)
+            {
+
+                if (arr[j].Salary > eMaxSal.Salary)
+                {
+                    eMaxSal = arr[j];
+                }
+            }
+            Console.WriteLine("Highest Salary Emplloyee : " + eMaxSal.ToString());
+        }
+
+        public static void displayEmployee(Emplyee[] arr)
+        {
+            for (int j = 0; j < arr.Length; j++)
+            {
+                Console.WriteLine(arr[j].ToString());
+            }
+        }
+
+        public static void takeInput(Emplyee[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine("Enter Name for Employee {0}", i + 1);
+                string name = Console.ReadLine();
+                Console.WriteLine("Enter Salary for {0}", name);
+                int sal = Convert.ToInt32(Console.ReadLine());
+                Emplyee e = new Emplyee((i + 1), name, sal);
+                arr[i] = e;
+                Console.Clear();
+            }
+        }
+        public static void Main()
+        {
+            Emplyee[] arr = new Emplyee[5];
+            takeInput(arr);
+            HishestSalaryEmployee(arr);
+            displayEmployee(arr);
+        }
+    }
+}
+
